0006be7fd1671be26a7bec4f88de41681fcb185a
fara <fzn21@bath.ac.uk>
2023-06-08 16:53:06 +0100

    backend init

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/create-note/{note}")
+def create_note(note: str):
+    return {"note": note}
