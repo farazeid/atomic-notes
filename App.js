f34c007f0fa1ea0b7a3cd3619bcea83928a1f142
fara <fzn21@bath.ac.uk>
2023-06-08 12:34:04 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+    Pressable,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [inputHeight, setInputHeight] = useState("25%");
+    const [inputWidth, setInputWidth] = useState("25%");
+    // useEffect(() => {
+    //     const inputHeight = Math.max(25, input.length / 3).toString() + "%";
+    //     setInputHeight(inputHeight);
+    // }, [input]);
+
+    const [input, setInput] = useState(null);
+
+    const handleInputSubmit = () => {
+        console.log("Input:", input);
+        setInput("");
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TextInput
+                style={[
+                    styles.input,
+                    { height: inputHeight, width: inputWidth },
+                ]}
+                editable
+                multiline
+                placeholder="Note..."
+                placeholderTextColor="pink"
+                value={input === 0 ? input : null}
+                onChangeText={setInput}
+                onEndEditing={handleInputSubmit}
+            />
+            <Pressable
+                style={{ padding: 10, backgroundColor: "red" }}
+                onPress={handleInputSubmit}
+            >
+                <Text style={{ color: "white" }}>Submit</Text>
+            </Pressable>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        // backgroundColor: "red",
+    },
+
+    input: {
+        textAlign: "center",
+        textAlignVertical: "top",
+        paddingVertical: 30,
+        paddingHorizontal: 20,
+        color: "white",
+        backgroundColor: "blue",
+    },
 });
