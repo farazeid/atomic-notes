ff03383624d612daccce5cb704756e2f9a28f1c1
fara <fzn21@bath.ac.uk>
2023-06-16 11:59:32 +0100

    load db on startup & save db on shutdown

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from pydantic import BaseModel
+import numpy as np
+import os
+from fastapi.encoders import jsonable_encoder
 import uuid
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global note_all
+    file_path = "db.npy"
+    if os.path.getsize(file_path) == 0:
+        note_all = np.array([])
+    else:
+        note_all = np.load(file_path, allow_pickle=True)
+    print(f"startup, {note_all}")
+    yield
+    np.save("db.npy", note_all)
+    print(f"shutdown, {note_all}")
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,21 +38,19 @@ app.add_middleware(
 )
 
 
-note_all = []
-
-
 @app.get("/")
 async def read_root() -> None:
-    print("/")
+    print("/", note_all)
 
     return
 
 
 @app.get("/get-note-all")
 async def get_note_all() -> dict:
-    print(f"/get-note-all, {len(note_all)}")
+    # print(f"/get-note-all, {len(note_all)}")
+    print(f"/get-note-all, {note_all}")
 
-    return {"note_all": note_all}
+    return {"note_all": note_all.tolist()}
 
 
 class NoteCreate(BaseModel):
@@ -44,11 +61,12 @@ class NoteCreate(BaseModel):
 async def create_note(note: NoteCreate) -> dict:
     print(f"/post-note, {note.note}")
 
-    # num_notes += 1
-    note_all.append(
+    global note_all
+    note_all = np.append(
+        note_all,
         {
             "id": uuid.uuid4(),
             "note": note.note,
-        }
+        },
     )
     return {"message": "success"}
